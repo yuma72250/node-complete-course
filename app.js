2c867833b27b9a9ccc5e72201c0e97481f5dc203
Yuma <yuma.ollivier@hotmail.com>
2021-04-06 19:09:35 +0200

    create a raw html form and send a post request

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-const http = require('http');
+const bodyParser = require('body-parser');
 
-const server = http.createServer();
+// express import
+const express = require('express');
 
-server.listen(3000);
+// express app initializing
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/add-product', (req, res, next) => {
+    console.log('In add middleware');
+    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Send</button></form>')
+})
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>Hello from Express !</h1>')
+})
+
+app.listen(8000);
