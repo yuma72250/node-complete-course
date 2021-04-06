31d2e0633ff35cdea5a5dbf3dc4e3965e5583ffb
Yuma <yuma.ollivier@hotmail.com>
2021-04-06 19:21:07 +0200

    creation of the 'routes' folder and its admin and shop files

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
-const bodyParser = require('body-parser');
-
 // express import
 const express = require('express');
+
+const bodyParser = require('body-parser');
 
 // express app initializing
 const app = express();
 
+const adminRoutes = require('./routes/admin.js')
+const shopRoutes = require('./routes/shop.js')
+
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In add middleware');
-    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Send</button></form>')
-})
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/')
-})
-
-app.use('/', (req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>Hello from Express !</h1>')
-})
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(8000);
---
+++ routes/admin.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/add-product', (req, res, next) => {
+    console.log('In add middleware');
+    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Send</button></form>')
+})
+
+router.post('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+module.exports = router;
