d8ea1e8ba295aca6354d64db2042a68444b71ebf
Yuma <yuma.ollivier@hotmail.com>
2021-04-07 15:03:39 +0200

    creation of views files

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 // express import
 const express = require('express');
 
@@ -6,12 +8,16 @@ const bodyParser = require('body-parser');
 // express app initializing
 const app = express();
 
-const adminRoutes = require('./routes/admin.js')
-const shopRoutes = require('./routes/shop.js')
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
 
 app.listen(8000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,14 @@
+const path = require('path')
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    console.log('In add middleware');
-    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Send</button></form>')
+    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'))
 })
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body)
     res.redirect('/')
 })
