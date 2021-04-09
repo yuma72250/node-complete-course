15c00703e14e595fdb82027465ffaf4bb7203712
Yuma <yuma.ollivier@hotmail.com>
2021-04-09 17:45:52 +0200

    Order page added and update routes files

--- controllers/shop.js
+++ controllers/shop.js
@@ -29,6 +29,14 @@ exports.getCart = (req, res, next) => {
         pageTitle: 'Your Cart'
     })
 }
+
+exports.getOrders = (req, res, next) => {
+    res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders'
+    })
+}
+
 exports.getCheckout = (req, res, next) => {
     res.render('shop/checkout', {
         path: '/checkout',
--- models/products.js
+++ models/products.js
@@ -18,8 +18,11 @@ const getProductsFromFile = cb => {
 }
 
 module.exports = class Product{
-    constructor(t){
-        this.title = t;
+    constructor(title, imgUrl, description, price){
+        this.title = title;
+        this.imgUrl = imgUrl;
+        this.description = description;
+        this.price = price
     }
 
     save(){
