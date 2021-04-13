bcbca2b964c2f7e90f76b58898c8078561db1717
Yuma <yuma.ollivier@hotmail.com>
2021-04-13 08:15:38 +0200

    Deleting Cart Items

--- controllers/admin.js
+++ controllers/admin.js
@@ -59,5 +59,7 @@ exports.postEditProduct = (req, res, next) => {
 }
 
 exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId
+    const prodId = req.body.productId;
+    Product.deleteById(prodId)
+    res.redirect('/admin/products')
 }
--- models/products.js
+++ models/products.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 
+const Cart = require('./cart.js')
+
 const p = path.join(
     path.dirname(require.main.filename), 
     'data', 
@@ -46,10 +48,11 @@ module.exports = class Product{
 
     static deleteById(id){
         getProductsFromFile(products => {
+            const product = products.find(prod => prod.id === id)
             const updatedProducts = products.filter(prod => prod.id !== id)
             fs.writeFile(p, JSON.stringify(updatedProducts), err => {
                 if(!err){
-                    
+                    Cart.deleteProduct(id, product.price)
                 }
             })
         }
