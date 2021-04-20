c6095be4562f7b6b32a19bdc7479f8eda3ae5193
Yuma <yuma.ollivier@hotmail.com>
2021-04-20 18:39:10 +0200

    Adding an Order Model

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const Product = require('./models/products');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 app.use((req, res, next) => {
   User.findByPk(1)
@@ -45,6 +47,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem })
 
 sequelize
   .sync()
