73740b352528c1bcc38cea7dd4b7fa91b9cdf3e0
Yuma <yuma.ollivier@hotmail.com>
2021-04-23 10:11:58 +0200

    Working on the Shopping Cart

--- models/user.js
+++ models/user.js
@@ -19,6 +19,29 @@ const userSchema = new Schema({
   },
 });
 
+userSchema.methods.addToCart = function (product) {
+  const cartProductIndex = this.cart.items.findIndex(item => {
+    return item.productId.toString() === product._id.toString();
+  });
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: 1,
+    });
+    const updatedCart = {
+      items: updatedCartItems,
+    };
+    this.cart = updatedCart;
+    return this.save();
+  }
+};
+
 module.exports = mongoose.model('User', userSchema);
 
 // const getDb = require('../util/database').getDb;
