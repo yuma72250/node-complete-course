59ff85df698fd047fc8f37df0cfec5b828b8ac17
Yuma <yuma.ollivier@hotmail.com>
2021-04-27 18:13:29 +0200

    Adding Additional Flash Messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,19 +3,32 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
     isAuthenticated: false,
-    errorMessage: req.flash('error'),
+    errorMessage: message,
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
     isAuthenticated: false,
+    errorMessage: message,
   });
 };
 
@@ -37,6 +50,7 @@ exports.postLogin = (req, res, next) => {
             res.redirect('/');
           });
         }
+        req.flash('error', 'Invalid email or password.');
         res.redirect('/login');
       });
     })
@@ -50,6 +64,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
+        req.flash('error', 'Email exists already, please pick a different one.');
         res.redirect('/signup');
       }
       return bcrypt
