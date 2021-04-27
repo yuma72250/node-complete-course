1690f25ab2bb8dbc63f378cbddb5dc51aa0ac29d
Yuma <yuma.ollivier@hotmail.com>
2021-04-27 12:14:11 +0200

    Using Middleware to Protect Routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,20 @@
 const express = require('express');
 
 const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth')
 
 const router = express.Router();
 
-router.get('/add-product', adminController.getAddProducts);
+router.get('/add-product', isAuth, adminController.getAddProducts);
 
-router.get('/products', adminController.getProducts);
+router.get('/products', isAuth, adminController.getProducts);
 
-router.post('/add-product', adminController.postAddProducts);
+router.post('/add-product', isAuth, adminController.postAddProducts);
 
-router.get('/edit-product/:productId', adminController.getEditProducts);
+router.get('/edit-product/:productId', isAuth, adminController.getEditProducts);
 
-router.post('/edit-product', adminController.postEditProduct);
+router.post('/edit-product', isAuth, adminController.postEditProduct);
 
-router.post('/delete-product', adminController.postDeleteProduct);
+router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
