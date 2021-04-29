c683ac40045c818dfcf2f3378557bad3754ada24
Yuma <yuma.ollivier@hotmail.com>
2021-04-29 11:06:24 +0200

    Adding Conditional CSS Classes

--- controllers/auth.js
+++ controllers/auth.js
@@ -27,6 +27,11 @@ exports.getLogin = (req, res, next) => {
     pageTitle: 'Login',
     isAuthenticated: false,
     errorMessage: message,
+    oldInput: {
+      email: '',
+      password: '',
+    },
+    validationErrors: [],
   });
 };
 
@@ -47,27 +52,35 @@ exports.getSignup = (req, res, next) => {
       password: '',
       confirmPassword: '',
     },
+    validationErrors: [],
   });
 };
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).render('auth/login', {
       path: '/login',
       pageTitle: 'Login',
       errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+      },
+      validationErrors: errors.array(),
     });
   }
+
   User.findOne({ email: email })
     .then(user => {
       if (!user) {
         req.flash('error', 'Invalid email or password.');
         return res.redirect('/login');
       }
-      return bcrypt.compare(password, user.password).then(doMatch => {
+      bcrypt.compare(password, user.password).then(doMatch => {
         if (doMatch) {
           req.session.isLoggedIn = true;
           req.session.user = user;
@@ -97,6 +110,7 @@ exports.postSignup = (req, res, next) => {
         password: password,
         confirmPassword: req.body.confirmPassword,
       },
+      validationErrors: errors.array(),
     });
   }
   return bcrypt
