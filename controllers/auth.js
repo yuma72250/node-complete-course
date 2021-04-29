d03bf7c2131904694acb95d7efd688d49cfc190f
Yuma <yuma.ollivier@hotmail.com>
2021-04-29 11:16:48 +0200

    Adding Validation to Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -77,8 +77,16 @@ exports.postLogin = (req, res, next) => {
   User.findOne({ email: email })
     .then(user => {
       if (!user) {
-        req.flash('error', 'Invalid email or password.');
-        return res.redirect('/login');
+        return res.status(422).render('auth/login', {
+          path: '/login',
+          pageTitle: 'Login',
+          errorMessage: 'Invalid email or password.',
+          oldInput: {
+            email: email,
+            password: password,
+          },
+          validationErrors: [],
+        });
       }
       bcrypt.compare(password, user.password).then(doMatch => {
         if (doMatch) {
@@ -89,8 +97,16 @@ exports.postLogin = (req, res, next) => {
             res.redirect('/');
           });
         }
-        req.flash('error', 'Invalid email or password.');
-        res.redirect('/login');
+        return res.status(422).render('auth/login', {
+          path: '/login',
+          pageTitle: 'Login',
+          errorMessage: 'Invalid email or password.',
+          oldInput: {
+            email: email,
+            password: password,
+          },
+          validationErrors: [],
+        });
       });
     })
     .catch(err => console.log(err));
