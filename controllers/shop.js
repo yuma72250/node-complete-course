7c511f6466410c6fd80d347cd06355ba124a5fa2
Yuma <yuma.ollivier@hotmail.com>
2021-05-16 11:38:50 +0200

    Testricting Access and Stream Data

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,7 @@
 const Product = require('../models/product');
 const Order = require('../models/order');
 const fs = require('fs');
+const path = require('path');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -156,17 +157,25 @@ exports.getInvoice = (req, res, next) => {
       return next(new Error('Unauthorized !'));
     }
     const invoiceName = 'invoice' + orderId + '.pdf';
-    const invoicePath = fs.join('data', 'invoices', invoiceName);
-    fs.readFile(invoicePath, (err, data) => {
-      if (err) {
-        return next(err);
-      }
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader(
-        'Content-Disposition',
-        'inline; filename="' + invoiceName + '"'
-      );
-      res.send(data);
-    });
-  });
+    const invoicePath = path.join('data', 'invoices', invoiceName);
+    // fs.readFile(invoicePath, (err, data) => {
+    //   if (err) {
+    //     return next(err);
+    //   }
+    //   res.setHeader('Content-Type', 'application/pdf');
+    //   res.setHeader(
+    //     'Content-Disposition',
+    //     'inline; filename="' + invoiceName + '"'
+    //   );
+    //   res.send(data);
+    // });
+    const file = fs.createReadStream(invoicePath);
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader(
+      'Content-Disposition',
+      'inline; filename="' + invoiceName + '"'
+    );
+    file.pipe(res)
+  })
+  .catch(err => next(err));
 };
