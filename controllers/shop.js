20048c8e22cbe50250291302db9ed3646fafe49d
Yuma <yuma.ollivier@hotmail.com>
2021-05-19 08:16:47 +0200

    Preparing Pagination Data on the Server

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,9 +1,12 @@
-const Product = require('../models/product');
-const Order = require('../models/order');
 const fs = require('fs');
 const path = require('path');
 
 const PDFDocument = require('pdfkit');
+
+const Product = require('../models/product');
+const Order = require('../models/order');
+
+const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -39,12 +42,28 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
+        totalProducts: totalItmes,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch(err => {
@@ -179,17 +198,19 @@ exports.getInvoice = (req, res, next) => {
       let totalPrice = 0;
       order.products.forEach(prod => {
         totalPrice += prod.quantity * prod.product.price;
-        pdfDoc.fontSize(14).text(
-          prod.product.title +
-            ' - ' +
-            prod.quantity +
-            ' x ' +
-            '$' +
-            prod.product.price
-        );
+        pdfDoc
+          .fontSize(14)
+          .text(
+            prod.product.title +
+              ' - ' +
+              prod.quantity +
+              ' x ' +
+              '$' +
+              prod.product.price
+          );
       });
-      pdfDoc.text('--------')
-      pdfDoc.fontSize(20).text('Total price : $' + totalPrice)
+      pdfDoc.text('--------');
+      pdfDoc.fontSize(20).text('Total price : $' + totalPrice);
       pdfDoc.end();
       // fs.readFile(invoicePath, (err, data) => {
       //   if (err) {
